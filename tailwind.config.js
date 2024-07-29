ddcd0095584e39833f1b1348f9a8541dea45b6d3
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-07-29 16:40:49 -0300

    add desktop backgroud to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'desktop': "url('/src/assets/images/background/background-desktop.svg')",
+      }
+    },
   },
   plugins: [],
 }
