5912e3f8ae979647e411fee8f1d73ecbfa5b485d
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:32:22 -0300

    add display flex to home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/images/icons/logo.svg';
 
 function Home() {
   return (
-    <div className="h-screen bg-mobile bg-center bg-cover">
+    <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
       <div className="w-[95%] bg-gradient-to-b from-governor_bay to-governor_bay_transparent">
         <img src={logo} alt="" />
       </div>
