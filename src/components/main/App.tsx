8f0c874c0264c7a4f495a802aa1f4f4b58eba109
leonardo <leocoelho.pi@gmail.com>
2024-08-05 16:48:29 -0300

    change desktop background for mobile

--- src/components/main/App.tsx
+++ src/components/main/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="h-screen bg-desktop bg-center bg-cover">
+    <div className="h-screen bg-mobile bg-center bg-cover">
       
     </div>
   );
