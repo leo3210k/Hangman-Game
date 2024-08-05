be8475815e9ecea4d8e92a745c670fe57d74d95b
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:07:51 -0300

    add logo to app

--- src/components/main/App.tsx
+++ src/components/main/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import logo from '../../assets/images/icons/logo.svg';
+
 function App() {
   return (
     <div className="h-screen bg-mobile bg-center bg-cover">
+      <img src={logo} alt="" />
       
     </div>
   );
