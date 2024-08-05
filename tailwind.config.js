7f1cec4d726f109a058999139ff5c8eedcd7c344
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:33:52 -0300

    add governor bay transparent to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        governor_bay: "#344ABA"
+        governor_bay: "rgb(52, 74, 186)",
+        governor_bay_transparent: "rgb(52, 74, 186, 0)"
+      },
+      boxShadow: {
+        home: "0px 6px 0px 8px #2463FF inset, 0px -8px 0px 4px #140E66 inset"
       },
       backgroundImage: {
         'desktop': "url('/src/assets/images/background/background-desktop.svg')",
