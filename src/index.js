9c3aa64399acab275badc333a98f4fd91ee6a316
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:13:32 -0300

    update app import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/main/App.tsx';
+import App from './main/App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
