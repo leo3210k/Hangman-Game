6fe178aa2c2094c8bb64b7d54b13b1c5375f0c67
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:32:08 -0300

    set home container width

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import logo from '../assets/images/icons/logo.svg';
 function Home() {
   return (
     <div className="h-screen bg-mobile bg-center bg-cover">
-      <div className="bg-gradient-to-b from-governor_bay to-governor_bay_transparent">
+      <div className="w-[95%] bg-gradient-to-b from-governor_bay to-governor_bay_transparent">
         <img src={logo} alt="" />
       </div>
       
