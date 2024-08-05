551e644a1b49065dc710c458bbfb8c006eebc32c
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:36:59 -0300

    add play icon on home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import logo from '../assets/images/icons/logo.svg';
+import play from '../assets/images/icons/icon-play.svg';
 
 function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
       <div className="w-[90%] flex justify-center items-center bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-full">
         <img src={logo} alt="logo" />
+        <img src={play} alt="play" />
       </div>
       
     </div>
