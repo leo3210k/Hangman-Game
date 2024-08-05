45b8721f1973c12bf26d8b2a9a54566a84a48a99
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:32:33 -0300

    decrease home width

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import logo from '../assets/images/icons/logo.svg';
 function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
-      <div className="w-[95%] bg-gradient-to-b from-governor_bay to-governor_bay_transparent">
+      <div className="w-[90%] bg-gradient-to-b from-governor_bay to-governor_bay_transparent">
         <img src={logo} alt="" />
       </div>
       
