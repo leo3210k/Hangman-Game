d203de438afbc6839ae4304329bd48cdd3abbd23
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:15:09 -0300

    create home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import logo from '../assets/images/icons/logo.svg';
+
+function Home() {
+  return (
+    <div className="h-screen bg-mobile bg-center bg-cover">
+      <img src={logo} alt="" />
+      
+    </div>
+  );
+}
+
+export default Home;
