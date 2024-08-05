5a1146d85c56868944a50a184829a443876b5948
leonardo <leocoelho.pi@gmail.com>
2024-08-05 20:40:03 -0300

    add blush pink color to tailwind

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,9 @@ function Home() {
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
       <div className="w-[90%] flex flex-col justify-center items-center bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-full">
         <img src={logo} alt="logo" />
-        <img src={play} alt="play" className="p-16" />
+        <button className="p-16 rounded-full">
+          <img src={play} alt="play" />
+        </button>
       </div>
       
     </div>
