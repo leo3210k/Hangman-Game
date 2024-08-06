283e773a4f50f46758d3a2ab8d6b3db3318e1226
leonardo <leocoelho.pi@gmail.com>
2024-08-06 16:18:49 -0300

    change home container border radius

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ import play from '../assets/images/icons/icon-play.svg';
 function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
-      <div className="w-[90%] flex flex-col justify-center items-center bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-full">
+      <div className="w-[90%] flex flex-col justify-center items-center bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-[7.2rem]">
         <img src={logo} alt="logo" />
         <button className="p-10 rounded-full bg-gradient-to-b from-blush_pink to-malibu shadow-home_play">
           <img src={play} alt="play" />
