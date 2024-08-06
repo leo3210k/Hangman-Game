564e428fe5f9c25d1befb1a82db2cd62f3fa39e9
leonardo <leocoelho.pi@gmail.com>
2024-08-06 20:26:45 -0300

    add width to back button image

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -6,8 +6,8 @@ function HowToPlay() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
       <div>
-        <button className="bg-gradient-to-b from-blush_pink to-malibu rounded-full p-4">
-          <img src={back} alt="back" />
+        <button className="w-10 h-10 flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full p-1">
+          <img src={back} alt="back" className="w-6" />
         </button>
       </div>
     </div>
