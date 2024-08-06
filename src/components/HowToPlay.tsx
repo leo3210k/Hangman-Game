a1e0fd602feedddb03f295a6df4780ce8e867c61
leonardo <leocoelho.pi@gmail.com>
2024-08-06 20:37:31 -0300

    add flex to howtoplay container

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -5,7 +5,7 @@ import back from '../assets/images/icons/icon-back.svg';
 function HowToPlay() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
-      <div>
+      <div className="flex justify-between items-center">
         <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3">
           <img src={back} alt="back" className="w-6" />
         </button>
