385587ba36916baf511e092faacdbfd90c7ecb0a
leonardo <leocoelho.pi@gmail.com>
2024-08-06 20:36:31 -0300

    create how to play title

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -9,6 +9,7 @@ function HowToPlay() {
         <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3">
           <img src={back} alt="back" className="w-6" />
         </button>
+        <h1>how to play</h1>
       </div>
     </div>
   );
