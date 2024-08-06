29b417e294e98cd8534e6d82a3e2699b04456b43
leonardo <leocoelho.pi@gmail.com>
2024-08-06 17:10:16 -0300

    create how to play component

---
+++ src/components/HowToPlay.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HowToPlay() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
+      Hello!! :D
+    </div>
+  );
+}
+
+export default HowToPlay;
