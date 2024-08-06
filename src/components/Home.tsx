a04d0d9082482359300037e1e3b1b29ddcd09076
leonardo <leocoelho.pi@gmail.com>
2024-08-06 16:30:48 -0300

    update home container logo position

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
       <div className="relative w-[90%] flex flex-col justify-center items-center gap-12 bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-[4.5rem] py-32">
-        <img src={logo} alt="logo" className="absolute -top-20" />
+        <img src={logo} alt="logo" className="absolute -top-24" />
         <button className="rounded-full bg-gradient-to-b from-blush_pink to-malibu shadow-home_play p-10">
           <img src={play} alt="play" />
         </button>
