5b6ba595ab27059b1a5927c8fa2ca8442a7de897
leonardo <leocoelho.pi@gmail.com>
2024-08-06 20:35:15 -0300

    add box shadow to back button

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -6,7 +6,7 @@ function HowToPlay() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
       <div>
-        <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full p-3">
+        <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3">
           <img src={back} alt="back" className="w-6" />
         </button>
       </div>
