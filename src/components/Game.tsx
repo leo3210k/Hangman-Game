f15641ec05dd74a270f4393d9f794e838ce06258
leonardo <leocoelho.pi@gmail.com>
2024-08-07 16:26:56 -0300

    add justify between to game header

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,7 +7,7 @@ function Game() {
   return (
     <div className="h-screen bg-gradient-to-b from-black_rock to-deep_koamaru px-6 pt-6 pb-8">
       <div className="flex flex-col gap-14">
-        <div className="w-full flex items-center gap-4">
+        <div className="w-full flex justify-between items-center gap-4">
           <div className="flex items-center gap-4">
             <Link to="/">
               <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3">
@@ -15,6 +15,9 @@ function Game() {
               </button>
             </Link>
             <h1 className="text-5xl text-white bg-clip-text">Movies</h1>
+          </div>
+          <div className="flex items-center gap-4">
+
           </div>
         </div>
       </div>
