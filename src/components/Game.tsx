23887242ad67a459942f9d1c15793f1138333f99
leonardo <leocoelho.pi@gmail.com>
2024-08-07 16:20:34 -0300

    remove justify between on game header and add gap

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,13 +7,13 @@ function Game() {
   return (
     <div className="h-screen bg-mobile bg-center bg-cover">
       <div className="flex flex-col gap-14 bg-gradient-to-b from-black_rock to-deep_koamaru px-6 pt-6 pb-8">
-        <div className="w-full flex justify-between items-center">
+        <div className="w-full flex items-center gap-4">
           <Link to="/">
             <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3">
               <img src={back} alt="back" className="w-6" />
             </button>
           </Link>
-          <h1 className="text-5xl text-transparent bg-clip-text tracking-wider bg-gradient-to-b from-malibu to-white">How to Play</h1>
+          <h1 className="text-5xl text-transparent bg-clip-text tracking-wider bg-gradient-to-b from-malibu to-white">Movies</h1>
         </div>
       </div>
     </div>
