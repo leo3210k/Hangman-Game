659344e4d830870e528ebad219c7f657990a590c
leonardo <leocoelho.pi@gmail.com>
2024-08-07 11:44:16 -0300

    create array categories

--- src/components/PickCategory.tsx
+++ src/components/PickCategory.tsx
@@ -4,6 +4,15 @@ import { Link } from 'react-router-dom';
 import back from '../assets/images/icons/icon-back.svg';
 
 function PickaCategory() {
+  const categories = [
+    "movies",
+    "tv shows",
+    "countries",
+    "capital cities",
+    "animals",
+    "sports"
+  ]
+
   return (
     <div className="h-screen bg-gradient-to-b from-black_rock to-deep_koamaru px-6 pt-6">
       <div className="flex flex-col gap-14">
@@ -13,7 +22,7 @@ function PickaCategory() {
               <img src={back} alt="back" className="w-6" />
             </button>
           </Link>
-          <h1 className="text-5xl text-transparent bg-clip-text tracking-wider bg-gradient-to-b from-malibu to-white">Pick a Category</h1>
+          <h1 className="text-[2.6rem] text-transparent bg-clip-text tracking-wider bg-gradient-to-b from-malibu to-white">Pick a Category</h1>
         </div>
       </div>
     </div>
