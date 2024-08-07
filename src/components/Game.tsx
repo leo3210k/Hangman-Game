071c86940cd4f62c1722ad958e7ee8ab068097e2
leonardo <leocoelho.pi@gmail.com>
2024-08-07 16:14:07 -0300

    create game component

---
+++ src/components/Game.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
+      
+    </div>
+  );
+}
+
+export default Game;
