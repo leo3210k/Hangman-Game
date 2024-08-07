475c2bcff0ce12cc713f1cdf78157519b9cd8752
leonardo <leocoelho.pi@gmail.com>
2024-08-06 21:25:10 -0300

    change itemlist title color

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -8,7 +8,7 @@ function HowToPlay() {
       <div className="flex flex-col gap-4 bg-white rounded-3xl p-8">
         <div className="flex items-center gap-8">
           <h2 className="text-2xl text-blue_ribbon">{ number }</h2>
-          <h3 className="text-2xl text-governor_bay tracking-wide uppercase">{ title }</h3>
+          <h3 className="text-2xl text-deep_koamaru tracking-wide uppercase">{ title }</h3>
         </div>
         <span className="text-purple_mountains_majesty tracking-wider leading-5">{ text }</span>
       </div>
