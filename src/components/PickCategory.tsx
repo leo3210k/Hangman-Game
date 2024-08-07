bc9f9acc6ba84923bf7ba4239a24f8a9f70fd99b
leonardo <leocoelho.pi@gmail.com>
2024-08-07 16:16:35 -0300

    add link to game on pickctegory

--- src/components/PickCategory.tsx
+++ src/components/PickCategory.tsx
@@ -27,7 +27,9 @@ function PickCategory() {
         <div className="flex flex-col gap-4">
           {categories.map(category => {
             return (
-              <button className="text-2xl text-white tracking-wider bg-blue_ribbon hover:bg-blue_ribbon_transparent rounded-full shadow-category uppercase py-5">{ category }</button>
+              <Link to="/game">
+                <button className="text-2xl text-white tracking-wider bg-blue_ribbon hover:bg-blue_ribbon_transparent rounded-full shadow-category uppercase py-5">{ category }</button>
+              </Link>
             )
           })}
         </div>
