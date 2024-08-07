add05e9b9e3fcf7c5f9e993d7f450ded48781f5c
leonardo <leocoelho.pi@gmail.com>
2024-08-07 16:24:51 -0300

    change back icon to menu icon

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import back from '../assets/images/icons/icon-back.svg'; 
+import menu from '../assets/images/icons/icon-menu.svg'; 
 
 function Game() {
   return (
@@ -10,7 +10,7 @@ function Game() {
         <div className="w-full flex items-center gap-4">
           <Link to="/">
             <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3">
-              <img src={back} alt="back" className="w-6" />
+              <img src={menu} alt="menu" className="w-6" />
             </button>
           </Link>
           <h1 className="text-5xl text-white bg-clip-text">Movies</h1>
