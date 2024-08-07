8fd53c707b72d63b5dceee35d102148b6cb444a9
leonardo <leocoelho.pi@gmail.com>
2024-08-06 21:14:13 -0300

    add to itemlist to howtoplay

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -23,6 +23,9 @@ function HowToPlay() {
         </button>
         <h1 className="text-5xl text-transparent bg-clip-text tracking-wide bg-gradient-to-b from-malibu to-white capitalize">how to play</h1>
       </div>
+      <ItemList number="01" title="Pick a category" text="First, choose a word category, like animals or movies. The computer then randomly selects a secret word from that topic and shows you blanks for each letter of the word."/>
+      <ItemList number="02" title="Guess letters" text="Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct. If it’s wrong, you lose some health, which empties after eight incorrect guesses."/>
+      <ItemList number="03" title="win or lose" text="You win by guessing all the letters in the word before your health runs out. If the health bar empties before you guess the word, you lose."/>
     </div>
   );
 }
