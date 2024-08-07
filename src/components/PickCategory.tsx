2b1bad935c4b98bc4e0558c558bb08e1b7dea45c
leonardo <leocoelho.pi@gmail.com>
2024-08-07 11:21:40 -0300

    create pickcategory component

---
+++ src/components/PickCategory.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PickaCategory() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
+      
+    </div>
+  );
+}
+
+export default PickaCategory;
