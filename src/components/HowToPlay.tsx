51d6ad7edbcc741f4765cc1ae6e78c2a7e6cf30e
leonardo <leocoelho.pi@gmail.com>
2024-08-06 21:28:46 -0300

    change padding from howtoplay to howtoplay container

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -16,8 +16,8 @@ function HowToPlay() {
   }
 
   return (
-    <div className="h-screen bg-mobile bg-center bg-cover px-6 pt-6">
-      <div className="flex flex-col gap-14 bg-gradient-to-b from-blush_pink to-malibu">
+    <div className="h-screen bg-mobile bg-center bg-cover">
+      <div className="flex flex-col gap-14 bg-gradient-to-b from-blush_pink to-malibu px-6 pt-6">
         <div className="w-full flex justify-between items-center">
           <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3">
             <img src={back} alt="back" className="w-6" />
