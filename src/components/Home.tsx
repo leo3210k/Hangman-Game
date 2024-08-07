ef2679d99a4408c22b02336b87590091c712d690
leonardo <leocoelho.pi@gmail.com>
2024-08-07 11:17:01 -0300

    add margin to home container

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ import play from '../assets/images/icons/icon-play.svg';
 function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-mobile bg-center bg-cover">
-      <div className="relative w-[90%] flex flex-col justify-center items-center gap-12 bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-[4.5rem] py-32">
+      <div className="relative w-[90%] flex flex-col justify-center items-center gap-12 bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-[4.5rem] -mb-20 py-32">
         <img src={logo} alt="logo" className="absolute -top-24" />
         <button className="rounded-full bg-gradient-to-b from-blush_pink to-malibu shadow-home_play p-10">
           <img src={play} alt="play" />
