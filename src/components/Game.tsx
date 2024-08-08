c16d62b6a054f10d9a20705a3b22e046e9270f7f
leonardo <leocoelho.pi@gmail.com>
2024-08-08 11:52:29 -0300

    remove ternary operator

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Game.css';
 import menu from '../assets/images/icons/icon-menu.svg'; 
 import heart from '../assets/images/icons/icon-heart.svg'; 
+import hangmanData from '../assets/json/words.json';
 
 function Game() {
+  const data: HangmanData = hangmanData;
+
+  function getRandomWord(category: keyof HangmanWords): string {
+    const words = data.words[category];
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  }
+
+  const location = useLocation();
+
+  const category = location.state.some as keyof HangmanWords;
+  const randomMovie = getRandomWord(category);
+
   return (
     <div className="h-screen bg-gradient-to-b from-black_rock to-deep_koamaru px-6 pt-6 pb-8">
       <div className="flex flex-col gap-14">
@@ -22,6 +36,16 @@ function Game() {
             <progress value="100" max="100" className="w-16 h-3 bg-white rounded-full p-[2px]"></progress>
             <img src={heart} alt="heart" className="w-10" />
           </div>
+        </div>
+        <div className="flex flex-wrap">
+          {randomMovie
+            .replace(/_/g, " ")
+            .split("")
+            .map((letter, i) => {
+              return (
+                <div key={letter + i}>{ letter }</div>
+              )
+            })}
         </div>
       </div>
     </div>
