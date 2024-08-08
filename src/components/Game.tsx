2f06cc7ef125187bfb72d8121bfec397274ae729
leonardo <leocoelho.pi@gmail.com>
2024-08-08 20:00:20 -0300

    add padding to letters

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -54,7 +54,7 @@ function Game() {
               <div className="flex flex-wrap gap-2" key={word + i}>
                 {word.split("").map((letter, i) => {
                   return (
-                    <div className="animation text-4xl text-transparent bg-blue_ribbon rounded-full shadow-category uppercase" key={letter + i}
+                    <div className="animation text-4xl text-transparent bg-blue_ribbon rounded-full shadow-category uppercase px-4 py-4" key={letter + i}
                       data-letter={letter}>
                       {letter}
                     </div>
