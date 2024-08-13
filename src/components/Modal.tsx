cd7e58efe1df788a3785d574cc8cfa06c571d639
leonardo <leocoelho.pi@gmail.com>
2024-08-13 11:17:29 -0300

    create interface to modal props

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,7 +14,14 @@ import {
 import menu from "../assets/images/icons/icon-menu.svg";
 import Modes from "./utils/modes";
 
-function Modal({ openMenu, setOpenMenu, mode, playAgain }: { openMenu: boolean; setOpenMenu: any; mode: Modes; playAgain: void; }) {
+interface ModalProps {
+  openMenu: boolean; 
+  setOpenMenu: any; 
+  mode: Modes; 
+  playAgain: void;
+}
+
+function Modal({ openMenu, setOpenMenu, mode, playAgain }: ModalProps) {
   function currentTitle() {
     if (mode === Modes.Playing) return "Pause";
     else if (mode === Modes.Lost) return "You Lose";
