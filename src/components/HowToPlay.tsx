dc365bb797f49569e8ab7461695d78ace074491a
leonardo <leocoelho.pi@gmail.com>
2024-08-14 20:56:54 -0300

    remove justify between from howtoplay header

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -19,7 +19,7 @@ function HowToPlay() {
   return (
     <div className="min-h-svh bg-mobile sm:bg-tablet bg-center bg-cover">
       <div className="flex flex-col gap-14 bg-gradient-to-b from-black_rock to-deep_koamaru px-6 pt-6 pb-8">
-        <div className="w-full flex justify-between items-center">
+        <div className="w-full flex justify-between sm:justify-normal items-center">
           <Link to="/">
             <button className="flex justify-center items-center bg-gradient-to-b from-blush_pink to-malibu rounded-full shadow-howtoplay_back p-3 sm:p-4">
               <img src={back} alt="back" className="w-6 sm:w-10" />
