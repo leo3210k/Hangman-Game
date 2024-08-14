b86af3bbf3081b2ac46a2e74d4c34151e2f1b779
leonardo <leocoelho.pi@gmail.com>
2024-08-14 20:04:57 -0300

    change text size to alphabet

--- src/components/Alphabet.tsx
+++ src/components/Alphabet.tsx
@@ -7,7 +7,7 @@ function Alphabet({ onClick }: { onClick: any }) {
     <div className="flex justify-center items-center flex-wrap gap-3">
       {alphabet.map((letter, i) => {
         return (
-          <button className="text-2xl sm:text-4xl text-deep_koamaru sm:font-medium bg-white rounded-full sm:rounded-[2rem] px-4 sm:px-8 py-3 sm:py-6" key={i + letter}
+          <button className="text-2xl sm:text-5xl text-deep_koamaru sm:font-medium bg-white rounded-full sm:rounded-[2rem] px-4 sm:px-8 py-3 sm:py-6" key={i + letter}
             onClick={e => onClick(e)}>
             {letter}
           </button>
