491b4838d083f8740fd200f9ebe879fdbbbcc7f8
leonardo <leocoelho.pi@gmail.com>
2024-08-14 17:09:23 -0300

    add grid to categories

--- src/components/PickCategory.tsx
+++ src/components/PickCategory.tsx
@@ -24,7 +24,7 @@ function PickCategory() {
           </Link>
           <h1 className="text-[2.6rem] sm:text-[6.5rem] text-transparent bg-clip-text tracking-wider sm:tracking-normal bg-gradient-to-b from-malibu to-white">Pick a Category</h1>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 sm:grid sm:grid-cols-2">
           {categories.map(category => {
             return (
               <Link to="/game" state={{ category: category.replace(" ", "_") }} key={category}>
