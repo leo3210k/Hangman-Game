0239b762a3e2beb914be27c8521e4f85ea53199a
leonardo <leocoelho.pi@gmail.com>
2024-08-14 20:04:22 -0300

    add font weight to alphabet

--- src/components/Alphabet.tsx
+++ src/components/Alphabet.tsx
@@ -7,7 +7,7 @@ function Alphabet({ onClick }: { onClick: any }) {
     <div className="flex justify-center items-center flex-wrap gap-3">
       {alphabet.map((letter, i) => {
         return (
-          <button className="text-2xl sm:text-4xl text-deep_koamaru bg-white rounded-full sm:rounded-[2.4rem] px-4 sm:px-8 py-3 sm:py-8" key={i + letter}
+          <button className="text-2xl sm:text-4xl text-deep_koamaru sm:font-medium bg-white rounded-full sm:rounded-[2.4rem] px-4 sm:px-8 py-3 sm:py-8" key={i + letter}
             onClick={e => onClick(e)}>
             {letter}
           </button>
