2196474919502cf8b21d6d96477f6a4f387f3c07
leonardo <leocoelho.pi@gmail.com>
2024-08-14 20:10:24 -0300

    add gap between letters

--- src/components/Letters.tsx
+++ src/components/Letters.tsx
@@ -5,7 +5,7 @@ function Letters({ chosenWord }: { chosenWord: string | null }) {
     <div className="flex justify-center items-center flex-wrap gap-y-4 gap-x-6">
       {chosenWord?.split("_").map((word, i) => {
         return (
-          <div className="flex flex-wrap gap-2" key={word + i}>
+          <div className="flex flex-wrap gap-2 sm:gap-4" key={word + i}>
             {word.split("").map((letter, i) => {
               return (
                 <div className="animation text-4xl text-transparent bg-blue_ribbon rounded-full shadow-category uppercase px-4 sm:px-7 py-4 sm:py-8" key={letter + i}
