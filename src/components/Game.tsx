6ca6c3f20ea79b8b0610b06b91b88913ff9fbb8c
leonardo <leocoelho.pi@gmail.com>
2024-08-15 16:56:40 -0300

    add text size to game title

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -130,7 +130,7 @@ function Game() {
         <div className="w-full flex justify-between items-center gap-4">
           <div className="flex items-center gap-4 sm:gap-10">
             <Modal openMenu={openMenu} setOpenMenu={setOpenMenu} mode={mode} playAgain={resetGame}/>
-            <h1 className="text-5xl text-white bg-clip-text capitalize">{category.replace("_", " ")}</h1>
+            <h1 className="text-5xl lg:text-8xl text-white bg-clip-text capitalize">{category.replace("_", " ")}</h1>
           </div>
           <div className="flex items-center gap-4 sm:gap-7">
             <progress value="100" max="100" className="w-16 sm:w-24 h-3 sm:h-[0.85rem] bg-white rounded-full p-[2px]"></progress>
