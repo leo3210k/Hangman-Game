b585ebd77f3c2aef9e834c954ad5fc55aefafc7e
leonardo <leocoelho.pi@gmail.com>
2024-08-15 11:48:00 -0300

    add gap to items list

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -29,7 +29,7 @@ function HowToPlay() {
           </Link>
           <h1 className="text-5xl sm:text-[6.5rem] text-transparent bg-clip-text tracking-wider sm:tracking-normal bg-gradient-to-b from-malibu to-white">How to Play</h1>
         </div>
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 sm:gap-8">
           <ItemList number="01" title="Pick a category" text="First, choose a word category, like animals or movies. The computer then randomly selects a secret word from that topic and shows you blanks for each letter of the word."/>
           <ItemList number="02" title="Guess letters" text="Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct. If it’s wrong, you lose some health, which empties after eight incorrect guesses."/>
           <ItemList number="03" title="win or lose" text="You win by guessing all the letters in the word before your health runs out. If the health bar empties before you guess the word, you lose."/>
