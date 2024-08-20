97bb08f07d43fa4b0dda8f9cc4b313dc947b7be2
leonardo <leocoelho.pi@gmail.com>
2024-08-19 21:37:14 -0300

    remove blank space

--- src/components/utils/dialog.tsx
+++ src/components/utils/dialog.tsx
@@ -18,7 +18,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 z-50 bg-gradient-to-b from-black_rock/80 to-deep_koamaru/70  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 bg-gradient-to-b from-black_rock/80 to-deep_koamaru/70 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
