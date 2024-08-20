577435a01c21f330ddd34173d9bdc750d8306803
leonardo <leocoelho.pi@gmail.com>
2024-08-19 21:22:59 -0300

    add animation to home play button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import logo from '../assets/images/icons/logo.svg';
 import play from '../assets/images/icons/icon-play.svg';
+import { motion } from 'framer-motion'
 
 function Home() {
   return (
@@ -10,12 +11,15 @@ function Home() {
       <div className="relative w-[90%] sm:w-[32rem] flex flex-col justify-center items-center gap-16 bg-gradient-to-b from-governor_bay to-governor_bay_transparent shadow-home_container rounded-[4.5rem] -mb-24 py-28">
         <img src={logo} alt="logo" className="absolute -top-28" />
         <Link to="/pickcategory">
-          <button className="rounded-full bg-gradient-to-b from-blush_pink to-malibu shadow-home_play p-10">
+          <motion.button className="rounded-full bg-gradient-to-b from-blush_pink to-malibu shadow-home_play p-10"
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: "spring", stiffness: 400, damping: 10 }}
+          >
             <img src={play} alt="play" />
-          </button>
+          </motion.button>
         </Link>
         <Link to="/howtoplay">
-          <button className="text-4xl text-white tracking-wide bg-blue_ribbon shadow-home_how rounded-full uppercase px-6 sm:px-16 py-3">
+          <button className="home_buttons text-4xl text-white tracking-wide bg-blue_ribbon shadow-home_how rounded-full uppercase px-6 sm:px-16 py-3">
             how to play
           </button>
         </Link>
