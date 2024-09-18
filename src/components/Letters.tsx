0f39b4386e9aeb4f79c50dbdcb055f227f44555b
leonardo <leocoelho.pi@gmail.com>
2024-09-17 21:29:37 -0300

    add to lower case on letters component

--- src/components/Letters.tsx
+++ src/components/Letters.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Letters({ chosenWord }: { chosenWord: string | null }) {
   return (
     <div className="flex justify-center items-center flex-wrap gap-y-4 sm:gap-y-6 gap-x-6 sm:gap-x-16">
-      {chosenWord?.split("_").map((word, i) => {
+      {chosenWord?.toLowerCase().split("_").map((word, i) => {
         return (
           <div className="flex flex-wrap gap-2 sm:gap-4" key={word + i}>
-            {word.split("").map((letter, i) => {
+            {word.toLowerCase().split("").map((letter, i) => {
               return (
                 <div className="animation sm:w-[4.25rem] sm:h-[6.25rem] lg:w-[5.5rem] lg:h-[8rem] sm:flex sm:justify-center sm:items-center text-4xl sm:text-6xl text-transparent bg-blue_ribbon rounded-full shadow-category uppercase px-4 sm:px-0 py-4 sm:py-0" key={letter + i}
                   data-letter={letter}>
